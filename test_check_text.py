5f2da6ae4858e1616a5642603d2013854b6c93e9
dmitriy78787878 <dimon798master@gmail.com>
2025-05-19 22:55:08 +0300

    Create test_check_text.py

---
+++ test_check_text.py
@@ -0,0 +1,14 @@
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+
+def test_check_footer(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    assert demo_qa_page.text_footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+def test_check_text_please(browser):
+    demo_qa_page = DemoQa(browser)
+    el_page = ElementsPage(browser)
+    demo_qa_page.visit()
+    demo_qa_page.btn_elements.click()
+    assert el_page.text_please.get_text() == 'Please select an item from left to start practice.'
